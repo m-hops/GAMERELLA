08c0362340ce5f28572d3fb5560f3c8a0b2a0cdb
rabbit-isdead <madeline.hops@gmail.com>
2020-11-14 15:36:27 -0500

    Loading assets into scenes
    Scenes are currently static, animation has not been added

--- untitled game/js/game/scene/Language.js
+++ untitled game/js/game/scene/Language.js
@@ -1,14 +1,10 @@
-
-
-
-class MainMenu extends Scene{
+class Language extends Scene{
   static background;
   static title;
-  static start;
   static onPreload(){
-    MainMenu.background = loadImage('assets/images/1. start/startSplash.png');
-    MainMenu.title = loadImage('assets/images/1. start/title.png');
-    MainMenu.start = loadImage('assets/images/1. start/startBtn.png');
+    Language.background = loadImage('assets/images/2. language/languageBKG.png');
+    Language.english = loadImage('assets/images/1. start/title.png');
+    Language.start = loadImage('assets/images/1. start/startBtn.png');
 
   }
 
@@ -20,7 +16,7 @@ class MainMenu extends Scene{
     // z at 0 will draw between -1 and 1
     // z at 1 will draw on top
     // z at -1 will draw bellow
-    SceneUtil.addImage(this, MainMenu.background, 0,0,0, 1920, 1080);
+    SceneUtil.addImage(this, Language.background, 0,0,0, 1920, 1080);
 
 
     // let myTextObject = SceneUtil.addText(this, "Allo Allo", color(0, 0, 255), 'arial', 175,200, 2, 500, 200); // z at -1 will draw bellow
